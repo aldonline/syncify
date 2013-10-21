448b04932020fcdfc61b48a84cf142c0f8fede01
Aldo Bucchi <aldo.bucchi@gmail.com>
2013-10-20 21:00:00 -0700

    Add JS bundle script. closes #12

---
+++ bundle.sh
@@ -0,0 +1,8 @@
+rm -r ./target
+rm -r ./build
+mkdir ./target
+mkdir ./build
+./node_modules/coffee-script/bin/coffee -c -o ./target ./lib/*.coffee
+./node_modules/browserify/bin/cmd.js ./target/index.js > ./build/syncify.js
+./node_modules/browserify/bin/cmd.js ./target/index.js | ./node_modules/uglify-js/bin/uglifyjs > ./build/syncify.min.js
+rm -r ./target
