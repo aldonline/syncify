56eb64f74fa21e23e6f0e4d1bcfbe848de709c49
aldonline <aldo.bucchi@gmail.com>
2013-12-23 13:31:09 -0800

    update bundle.sh to look in the /src folder ( everything used to live in in /lib but we separated it to /src and /lib )

--- bundle.sh
+++ bundle.sh
@@ -2,7 +2,7 @@ rm -r ./target
 rm -r ./build
 mkdir ./target
 mkdir ./build
-./node_modules/coffee-script/bin/coffee -c -o ./target ./lib/*.coffee
+./node_modules/coffee-script/bin/coffee -c -o ./target ./src/*.coffee
 ./node_modules/browserify/bin/cmd.js ./target/index.js > ./build/syncify.js
 ./node_modules/browserify/bin/cmd.js ./target/index.js | ./node_modules/uglify-js/bin/uglifyjs > ./build/syncify.min.js
 rm -r ./target
